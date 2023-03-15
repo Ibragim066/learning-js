acb51c5d292c5ac3ce754988a8f9c487acc91bd8
Ibragim <gamemega.ao@gmail.com>
2023-03-15 09:14:38 +0300

    switch

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 51:
+        console.log('в точку');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;
+}
