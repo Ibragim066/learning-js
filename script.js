fac00f5409ac73b52a7c8c05692806707fb1a823
Ibragim <gamemega.ao@gmail.com>
2023-03-20 13:24:39 +0300

    Array Tasks

--- script.js
+++ script.js
@@ -247,3 +247,51 @@ const a = showProgrammingLangs(personalPlanPeter);
 console.log(a);
 
 
+//Задача на работу с массивами
+
+// 1 Задача:
+// Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+
+// Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+
+    arr.forEach(member => {
+        str += `${member}, `
+    });
+
+    return str;
+}
+
+const names = showFamily(family);
+console.log(names);
+
+// 2 Задача:
+// Напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+
+// Пример:
+
+// standardizeStrings(favoriteCities)  выведет в консоль:
+
+// lisbon
+// rome
+// milan
+// dublin
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    arr.forEach(city => {
+        console.log(city.toLowerCase())
+    })
+}
+
+const cities = standardizeStrings(favoriteCities);
+console.log(cities);
