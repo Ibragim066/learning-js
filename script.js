abaa246e1633ca9c464f5136b2a0259579e40261
Ibragim <gamemega.ao@gmail.com>
2023-03-20 13:03:19 +0300

    objects

--- script.js
+++ script.js
@@ -1,195 +1,249 @@
 'use strict'
 
-// let numberOfFilms;
+// // let numberOfFilms;
 
-// function start() {
-//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// // function start() {
+// //     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// //     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+// //         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// //     }
+// // }
+
+// // // start();
+
+// // const personalMovieDB = {
+// //     count: numberOfFilms,
+// //     movies: {},
+// //     actors: {},
+// //     genres: [],
+// //     privat: false
+// // };
+
+// // for (let i = 0; i < 2; i++) {
+// //     const a = prompt('один из последних просмотренных фильмов?', ''),
+// //         b = prompt('На сколько оцените его?', '');
+
+// //     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// //         personalMovieDB.movies[a] = b;
+// //         console.log('done');
+// //     } else {
+// //         console.log('error');
+// //         i--;
+// //     }
+// // }
+
+// // function rememberMyFilms() {
+// //     for (let i = 0; i < 2; i++) {
+// //         const a = prompt('один из последних просмотренных фильмов?', '').trim(),
+// //             b = prompt('На сколько оцените его?', '');
+
+// //         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// //             personalMovieDB.movies[a] = b;
+// //             console.log('done');
+// //         } else {
+// //             console.log('error');
+// //             i--;
+// //         }
+// //     }
+// // }
+
+// // // rememberMyFilms();
+
+// // function detectPersonalLevel() {
+// //     if (personalMovieDB.count < 10) {
+// //         console.log("Просмотрено довльно мало фильмов");
+// //     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+// //         console.log("Вы классический зритель");
+// //     } else if (personalMovieDB.count >= 30) {
+// //         console.log("Вы киноман");
+// //     } else {
+// //         console.log("Ошибка");
+// //     }
+// // }
+
+// // // detectPersonalLevel();
+
+// // function showMyDB(hidden) {
+// //     if (!hidden) {
+// //         console.log(personalMovieDB);
+// //     }
+// // }
+
+// // // showMyDB(personalMovieDB.privat);
+
+// // function writeYourGenres() {
+// //     for (let i = 1; i <= 3; i++) {
+// //         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр по номерои ${i}`);
+// //     }
+// // }
+
+// // writeYourGenres();
+
+// // console.log(personalMovieDB);
+
+
+
+
+
+
+// // Функция, которая принимает в себя целое число минут и возвращает время в нужном формате строки.
+// function getTimeFromMinutes(minutesTotal) {
+//     if (typeof (minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+//         return "Ошибка, проверьте данные";
 //     }
+
+//     const hours = Math.floor(minutesTotal / 60);
+//     const minutes = minutesTotal % 60;
+
+//     switch (hours) {
+//         case 0:
+//             hoursStr = "часов";
+//             break;
+//         case 1:
+//             hoursStr = 'час';
+//             break;
+//         case 2:
+//         case 3:
+//         case 4:
+//             hoursStr = 'часа';
+//             break;
+//         default:
+//             hoursStr = 'часов';
+//     }
+
+//     switch (minutes) {
+//         case 0:
+//             minutesStr = "минут";
+//             break;
+//         case 1:
+//             minutesStr = "минута";
+//             break;
+//         case 2:
+//         case 3:
+//         case 4:
+//             minutesStr = "минуты"
+//             break;
+//         default:
+//             minutesStr = "минут"
+//     }
+
+//     return `Это ${hours} ${hoursStr} и ${minutes} ${minutesStr}`;
+
 // }
 
-// // start();
+// const result = getTimeFromMinutes(120);
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
+// console.log(result);
+
+
+// //      Задача с числами Фибоначчи
+// function fib(num) {
+//     if (typeof (num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+//         return "";
+//     }
+
+//     let result = '';
+//     let first = 0;
+//     let second = 1;
+
+//     for (let i = 0; i < num; i++) {
+//         if (i + 1 === num) {
+//             result += `${first}`;
+//             // Без пробела в конце
+//         } else {
+//             result += `${first} `;
+//         }
+
+
+//         let third = first + second;
+//         first = second;
+//         second = third;
+//     }
+
+//     return result;
+// }
+
+// const number = fib(7);
+// console.log(number);
+
+
+// // Objects 
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function () {
+//         console.log("Test");
+//     }
 // };
 
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
+// options.makeTest();
 
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-// }
+// console.log(Object.keys(options).length);
 
-// function rememberMyFilms() {
-//     for (let i = 0; i < 2; i++) {
-//         const a = prompt('один из последних просмотренных фильмов?', '').trim(),
-//             b = prompt('На сколько оцените его?', '');
+// // console.log(options.name);
 
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//             i--;
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${key} имеет значение ${options[key][i]}`);
 //         }
-//     }
-// }
-
-// // rememberMyFilms();
-
-// function detectPersonalLevel() {
-//     if (personalMovieDB.count < 10) {
-//         console.log("Просмотрено довльно мало фильмов");
-//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//         console.log("Вы классический зритель");
-//     } else if (personalMovieDB.count >= 30) {
-//         console.log("Вы киноман");
 //     } else {
-//         console.log("Ошибка");
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
 //     }
 // }
 
-// // detectPersonalLevel();
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function (plan) {
+        const { age } = plan;
+        const { languages } = plan.skills;
+        let str = `Мне ${age} и я владею языками: `;
 
-// function showMyDB(hidden) {
-//     if (!hidden) {
-//         console.log(personalMovieDB);
-//     }
-// }
+        languages.forEach(function (lang) {
+            str += `${lang.toUpperCase()} `;
+        });
 
-// // showMyDB(personalMovieDB.privat);
-
-// function writeYourGenres() {
-//     for (let i = 1; i <= 3; i++) {
-//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр по номерои ${i}`);
-//     }
-// }
-
-// writeYourGenres();
-
-// console.log(personalMovieDB);
-
-
-
-
-
-
-// Функция, которая принимает в себя целое число минут и возвращает время в нужном формате строки.
-function getTimeFromMinutes(minutesTotal) {
-    if (typeof (minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
-        return "Ошибка, проверьте данные";
-    }
-
-    const hours = Math.floor(minutesTotal / 60);
-    const minutes = minutesTotal % 60;
-
-    switch (hours) {
-        case 0:
-            hoursStr = "часов";
-            break;
-        case 1:
-            hoursStr = 'час';
-            break;
-        case 2:
-        case 3:
-        case 4:
-            hoursStr = 'часа';
-            break;
-        default:
-            hoursStr = 'часов';
-    }
-
-    switch (minutes) {
-        case 0:
-            minutesStr = "минут";
-            break;
-        case 1:
-            minutesStr = "минута";
-            break;
-        case 2:
-        case 3:
-        case 4:
-            minutesStr = "минуты"
-            break;
-        default:
-            minutesStr = "минут"
-    }
-
-    return `Это ${hours} ${hoursStr} и ${minutes} ${minutesStr}`;
-
-}
-
-const result = getTimeFromMinutes(120);
-
-console.log(result);
-
-
-//      Задача с числами Фибоначчи
-function fib(num) {
-    if (typeof (num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-        return "";
-    }
-
-    let result = '';
-    let first = 0;
-    let second = 1;
-
-    for (let i = 0; i < num; i++) {
-        if (i + 1 === num) {
-            result += `${first}`;
-            // Без пробела в конце
-        } else {
-            result += `${first} `;
-        }
-
-
-        let third = first + second;
-        first = second;
-        second = third;
-    }
-
-    return result;
-}
-
-const number = fib(7);
-console.log(number);
-
-
-// Objects 
-
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
+        return str;
     }
 };
 
-console.log(Object.keys(options).length);
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
 
-// console.log(options.name);
 
-for (let key in options) {
-    if (typeof (options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`Свойство ${key} имеет значение ${options[key][i]}`);
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+function showExperience(plan) {
+    const { exp } = plan.skills;
+    return exp;
+}
+
+showExperience(personalPlanPeter);
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const { programmingLangs } = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
     }
-} 
+
+    return str;
+}
+
+const a = showProgrammingLangs(personalPlanPeter);
+console.log(a);
+
+
